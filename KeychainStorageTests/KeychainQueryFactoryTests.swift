11339b283db7735209a9af86ede7341e0b377f7b
Dan, Radu-Ionut (RO - Bucharest) <rdan@deloittece.com>
2019-05-26 10:09:50 +0300

    Created unit tests

---
+++ KeychainStorageTests/KeychainQueryFactoryTests.swift
@@ -0,0 +1,53 @@
+//
+//  KeychainQueryFactoryTests.swift
+//  KeychainStorageTests
+//
+//  Created by Dan, Radu-Ionut (RO - Bucharest) on 26/05/2019.
+//  Copyright © 2019 radude89. All rights reserved.
+//
+
+import XCTest
+@testable import KeychainStorage
+
+final class KeychainQueryFactoryTests: XCTestCase {
+    
+    func test_makeQueryForService_setService() {
+        let expectedService = "com.test"
+        
+        let queryDictionary = KeychainBasicQueryFactory.makeQuery(forService: expectedService)
+        let service = queryDictionary[kSecAttrService as String] as! String
+        
+        XCTAssertEqual(service, expectedService)
+        XCTAssertGreaterThan(queryDictionary.count, 2)
+    }
+    
+    func test_makeQuery_setServiceAndKey() {
+        let expectedService = "com.test"
+        let expectedKey = "key.test"
+        
+        let queryDictionary = KeychainBasicQueryFactory.makeQuery(forService: expectedService, key: expectedKey)
+        let service = queryDictionary[kSecAttrService as String] as! String
+        let key = queryDictionary[kSecAttrAccount as String] as! String
+        
+        XCTAssertEqual(service, expectedService)
+        XCTAssertEqual(key, expectedKey)
+        XCTAssertGreaterThan(queryDictionary.count, 2)
+    }
+    
+    func test_makeQuery_setServiceAndKeyValue() {
+        let expectedService = "com.test"
+        let expectedKey = "key.test"
+        let expectedValue = "value.test".data(using: .utf8)!
+        
+        let queryDictionary = KeychainBasicQueryFactory.makeQuery(forService: expectedService, key: expectedKey, value: expectedValue)
+        let service = queryDictionary[kSecAttrService as String] as! String
+        let key = queryDictionary[kSecAttrAccount as String] as! String
+        let value = queryDictionary[kSecValueData as String] as! Data
+        
+        XCTAssertEqual(service, expectedService)
+        XCTAssertEqual(key, expectedKey)
+        XCTAssertEqual(value, expectedValue)
+        XCTAssertGreaterThan(queryDictionary.count, 3)
+    }
+
+}
--- KeychainStorageTests/KeychainStorageTests.swift
+++ KeychainStorageTests/KeychainStorageTests.swift
@@ -2,32 +2,136 @@
 //  KeychainStorageTests.swift
 //  KeychainStorageTests
 //
-//  Created by Dan, Radu-Ionut (RO - Bucharest) on 25/05/2019.
+//  Created by Dan, Radu-Ionut (RO - Bucharest) on 26/05/2019.
 //  Copyright © 2019 radude89. All rights reserved.
 //
 
 import XCTest
 @testable import KeychainStorage
 
-class KeychainStorageTests: XCTestCase {
-
+final class KeychainStorageTests: XCTestCase {
+    
+    var sut: KeychainStorage!
+    private let service = "com.test.service"
+    private let key = "test.key"
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        sut = KeychainStorage(service: service)
     }
-
+    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try? sut.removeAll()
+        super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_keychainWrapper_init() {
+        XCTAssertNotNil(sut)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_keychainWrapper_stringForKey() {
+        let expectedValue = "test.value"
+        
+        do {
+            try sut.set(expectedValue, key: key)
+            let actualValue = try sut.string(forKey: key)
+            XCTAssertEqual(actualValue, expectedValue)
+        } catch {
+            XCTFail("Thrown error \(error)")
+        }
+    }
+    
+    func test_keychainWrapper_stringForKey_isNil() {
+        XCTAssertNil(try sut.string(forKey: key))
+    }
+    
+    func test_keychainWrapper_boolForKey() {
+        do {
+            let expectedValue = true
+            try sut.set(expectedValue, key: key)
+            
+            let actualValue = try sut.bool(forKey: key)
+            
+            XCTAssertEqual(actualValue, expectedValue)
+        } catch {
+            XCTFail("Thrown error \(error)")
+        }
+    }
+    
+    func test_keychainWrapper_boolForKey_isNil() {
+        XCTAssertNil(try sut.bool(forKey: key))
+    }
+    
+    func test_keychainWrapper_removesStringValue() {
+        do {
+            let expectedValue = "test.value"
+            try sut.set(expectedValue, key: key)
+            
+            try sut.removeValue(forKey: key)
+            
+            XCTAssertNil(try sut.string(forKey: key))
+        } catch {
+            XCTFail("Thrown error \(error)")
+        }
+    }
+    
+    func test_keychainWrapper_removesBoolValue() {
+        do {
+            let expectedValue = false
+            try sut.set(expectedValue, key: key)
+            
+            try sut.removeValue(forKey: key)
+            
+            XCTAssertNil(try sut.bool(forKey: key))
+        } catch {
+            XCTFail("Thrown error \(error)")
+        }
+    }
+    
+    func test_keychainWrapper_overWritesStringValue() {
+        do {
+            let initialValue = "initial.test.value"
+            try sut.set(initialValue, key: key)
+            
+            let expectedValue = "test.value"
+            try sut.set(expectedValue, key: key)
+        }  catch {
+            XCTFail("Thrown error \(error)")
+        }
+    }
+    
+    func test_keychainWrapper_overwritesBoolValue() {
+        do {
+            let initialValue = false
+            try sut.set(initialValue, key: key)
+            
+            let expectedValue = true
+            try sut.set(expectedValue, key: key)
+            
+            let actualValue = try sut.bool(forKey: key)
+            
+            XCTAssertEqual(actualValue, expectedValue)
+        }  catch {
+            XCTFail("Thrown error \(error)")
+        }
+    }
+    
+    func test_keychainWrapper_removesValuesByService() {
+        do {
+            let expectedValue = "test.value"
+            let keychainWrapper = KeychainStorage(service: "\(service).two")
+            
+            try sut.set(expectedValue, key: key)
+            try keychainWrapper.set(expectedValue, key: key)
+            try sut.removeAll()
+            let actualValue = try keychainWrapper.string(forKey: key)
+            
+            XCTAssertNil(try sut.string(forKey: key))
+            XCTAssertEqual(actualValue, expectedValue)
+            
+            try keychainWrapper.removeAll()
+        }  catch {
+            XCTFail("Thrown error \(error)")
         }
     }
 
