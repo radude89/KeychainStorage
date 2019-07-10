ae822752819f5928d4df21d42ce5f0188d692ca1
Dan, Radu-Ionut (RO - Bucharest) <rdan@deloittece.com>
2019-07-10 13:18:30 +0100

    Updated UT

---
+++ KeychainStorageHostApp/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  KeychainStorageHostApp
+//
+//  Created by Dan, Radu-Ionut (RO - Bucharest) on 10/07/2019.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
